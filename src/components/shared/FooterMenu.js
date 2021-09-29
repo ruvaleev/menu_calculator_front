d900a64f4dbf3c958bf6123aaaff3420844d72a5
Ruslan Valeev <ruvaleev@gmail.com>
2021-09-29 18:10:03 +0700

    Add positioning to footer menu

--- src/components/shared/FooterMenu.js
+++ src/components/shared/FooterMenu.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import ButtonLink from '../shared/ButtonLink';
 
 function FooterMenu({ navigation }) {
   return (
-    <View testID='FooterMenu'>
+    <View testID='FooterMenu' style={styles.container}>
       <ButtonLink callback={() => navigation.navigate('Shopping List')} title='Shopping List' testID='Shopping List' />
       <ButtonLink callback={() => navigation.navigate('Receipts')} title='Receipts' testID='Receipts' />
       <ButtonLink callback={() => navigation.navigate('Menu')} title='Menu' testID='Menu' />
@@ -16,6 +16,17 @@ function FooterMenu({ navigation }) {
 }
 
 export default FooterMenu;
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-around'
+  },
+});
 
 FooterMenu.propTypes = {
   navigation: PropTypes.object.isRequired
